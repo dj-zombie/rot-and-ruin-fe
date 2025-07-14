f393413c137e806568ec80c6a742ece383af152d
dj-zombie <dj.zombie@protonmail.com>
2025-07-14 05:46:22 -0700

    (chore): updated product css

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,7 @@ import { globalIgnores } from "eslint/config";
 import { qwikEslint9Plugin } from "eslint-plugin-qwik";
 
 const ignores = [
+  ".tailwind.config.ts",
   "**/*.log",
   "**/.DS_Store",
   "**/*.",
---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  safelist: [
+    "h-[120]",
+    // Add all variants you use
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
